49ec44b896f12ada6ef4ba5480bfd8ca05007686
andy <ali@junglescout.com>
2022-05-09 13:49:01 +0800

    rename

--- part1/bin/firehose-s3.ts
+++ part1/bin/firehose-s3.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import "source-map-support/register";
-import { FirehoseToS3 } from "../lib/part1-stack";
+import { FirehoseToS3 } from "../lib/firehose-s3";
 
 const app = new cdk.App();
-new FirehoseToS3(app, "Part1Stack", {
+new FirehoseToS3(app, "FirehoseToS3Stack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
---
+++ part1/test/firehose-s3.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Part1 from '../lib/part1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/part1-stack.ts
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new Part1.Part1Stack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+});
